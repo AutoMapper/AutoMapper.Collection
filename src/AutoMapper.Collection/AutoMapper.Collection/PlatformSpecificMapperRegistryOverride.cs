ea762c8d5e79e49dc43412da3136b36f8b189bad
TylerCarlson1 <tycarlso@hotmail.com>
2015-04-20 23:34:08 -0500

    Internal Refactoring

---
+++ src/AutoMapper.Collection/AutoMapper.Collection/PlatformSpecificMapperRegistryOverride.cs
@@ -0,0 +1,27 @@
+﻿namespace AutoMapper.Mappers
+{
+    using System.Linq;
+
+    public class PlatformSpecificMapperRegistryOverride : IPlatformSpecificMapperRegistry
+    {
+        private readonly object _mapperLock = new object();
+
+        public void Initialize()
+        {
+            InsertBefore<ReadOnlyCollectionMapper>(new ExpressionOfTypeToExpressionOfDifferentTypeMapper());
+            InsertBefore<ReadOnlyCollectionMapper>(new ObjectToEquivalencyExpressionByEquivalencyExistingMapper());
+            InsertBefore<ReadOnlyCollectionMapper>(new EquivlentExpressionAddRemoveCollectionMapper());
+        }
+
+        private void InsertBefore<TObjectMapper>(IObjectMapper mapper)
+            where TObjectMapper : IObjectMapper
+        {
+            lock (_mapperLock)
+            {
+                var targetMapper = MapperRegistry.Mappers.FirstOrDefault(om => om is TObjectMapper);
+                var index = targetMapper == null ? 0 : MapperRegistry.Mappers.IndexOf(targetMapper);
+                MapperRegistry.Mappers.Insert(index, mapper);
+            }
+        }
+    }
+}
