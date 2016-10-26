ea1f41923fc89aa899b98f77529252f0139b812d
Tyler Carlson <TylerC@gyrodata.com>
2016-10-26 11:46:41 -0500

    Fix threading issues AGAIN

--- src/AutoMapper.Collection/AutoMapper.Collection/Equivilency Expression/ExpressionExtentions.cs
+++ src/AutoMapper.Collection/AutoMapper.Collection/Equivilency Expression/ExpressionExtentions.cs
@@ -1,39 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace AutoMapper.EquivilencyExpression
 {
     public static class ExpressionExtentions
     {
-        private static readonly IDictionary<Type, Type> _singleParameterTypeDictionary = new Dictionary<Type, Type>();
+        private static readonly ConcurrentDictionary<Type, Type> _singleParameterTypeDictionary = new ConcurrentDictionary<Type, Type>();
 
         public static Type GetSinglePredicateExpressionArgumentType(this Type type)
         {
-            if (_singleParameterTypeDictionary.ContainsKey(type))
-                return _singleParameterTypeDictionary[type];
+            return _singleParameterTypeDictionary.GetOrAdd(type, t =>
+            {
+                var isExpression = typeof(Expression).GetTypeInfo().IsAssignableFrom(t.GetTypeInfo());
+                if (!isExpression)
+                    return null;
 
-            var isExpression = typeof(Expression).IsAssignableFrom(type);
-            if (!isExpression)
-                return CacheAndReturnType(type, null);
+                var expressionOf = t.GetTypeInfo().GenericTypeArguments.First();
+                var isFunction = expressionOf.GetGenericTypeDefinition() == typeof(Func<,>);
+                if (!isFunction)
+                    return null;
 
-            var expressionOf = type.GetGenericArguments().First();
-            var isFunction = expressionOf.GetGenericTypeDefinition() == typeof(Func<,>);
-            if (!isFunction)
-                return CacheAndReturnType(type, null);
+                var isPredicate = expressionOf.GetTypeInfo().GenericTypeArguments[1] == typeof(bool);
+                if (!isPredicate)
+                    return null;
 
-            var isPredicate = expressionOf.GetGenericArguments()[1] == typeof(bool);
-            if (!isPredicate)
-                return CacheAndReturnType(type, null);
-
-            var objType = expressionOf.GetGenericArguments().First();
-            return CacheAndReturnType(type, objType);
+                var objType = expressionOf.GetTypeInfo().GenericTypeArguments.First();
+                return CacheAndReturnType(type, objType);
+            });
         }
 
         private static Type CacheAndReturnType(Type type, Type objType)
         {
-            _singleParameterTypeDictionary.Add(type, objType);
+            _singleParameterTypeDictionary.AddOrUpdate(type, objType, (t, t2) => objType);
             return objType;
         }
     }
