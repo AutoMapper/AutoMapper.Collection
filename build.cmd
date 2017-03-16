68a94331106fa9c56198113bfce44df936874050
Tyler Carlson <TylerC@gyrodata.com>
2017-03-16 16:31:17 -0500

    Build Push Nuget

--- build.cmd
+++ build.cmd
@@ -3,4 +3,3 @@ cd %~dp0
 
 @ECHO OFF
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\tools\psake\psake.psm1'; invoke-psake .\build.ps1 %*; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode} }"
-
