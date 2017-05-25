e54297ee110276c8650d8a676a752b573e9feaea
Tyler Carlson <tycarlso@hotmail.com>
2017-05-24 22:36:37 -0500

    better build

--- build.ps1
+++ build.ps1
@@ -41,6 +41,8 @@ task compile -depends clean {
 
     exec { .\.nuget\NuGet.exe restore $base_dir\AutoMapper.Collection.sln }
 
+    exec { dotnet restore $base_dir\AutoMapper.Collection.sln }
+
     exec { dotnet build $base_dir\AutoMapper.Collection.sln -c $config --version-suffix=$buildSuffix -v q /nologo }
 
 	exec { dotnet pack $source_dir\AutoMapper.Collection -c $config --include-symbols --no-build --output $artifacts_dir --version-suffix $suffix}
