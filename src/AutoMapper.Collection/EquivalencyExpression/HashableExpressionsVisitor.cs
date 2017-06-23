ba5e34cedf315d1fb0bf482afe64a73b96af680d
Tyler Carlson <tycarlso@hotmail.com>
2017-06-23 14:20:30 -0500

    Improve mapping performance

--- src/AutoMapper.Collection/EquivalencyExpression/HashableExpressionsVisitor.cs
+++ src/AutoMapper.Collection/EquivalencyExpression/HashableExpressionsVisitor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace AutoMapper.EquivalencyExpression
@@ -10,6 +11,8 @@ namespace AutoMapper.EquivalencyExpression
         private readonly ParameterExpression _destinationParameter;
         private readonly List<Expression> _sourceMembers = new List<Expression>();
         private readonly ParameterExpression _sourceParameter;
+
+        private readonly ParameterFinderVisitor _paramFinder = new ParameterFinderVisitor();
 
         internal HashableExpressionsVisitor(ParameterExpression sourceParameter, ParameterExpression destinationParameter)
         {
@@ -29,30 +32,52 @@ namespace AutoMapper.EquivalencyExpression
             switch (node.NodeType)
             {
                 case ExpressionType.Equal:
+                    VisitCompare(node.Left, node.Right);
+                    break;
                 case ExpressionType.And:
                 case ExpressionType.AndAlso:
                     return base.VisitBinary(node);
-            }
-            
-            return node;
-        }
-
-        protected override Expression VisitMember(MemberExpression node)
-        {
-            if (node.Expression == _sourceParameter)
-            {
-                _sourceMembers.Add(node);
-            }
-            else if (node.Expression == _destinationParameter)
-            {
-                _destinationMembers.Add(node);
+                case ExpressionType.Or:
+                case ExpressionType.OrElse:
+                    return node; // Maybe compare 0r's for expression matching on side
             }
 
             return node;
         }
 
-        protected override Expression VisitUnary(UnaryExpression node)
+        private void VisitCompare(Expression leftNode, Expression rightNode)
         {
+            _paramFinder.Visit(leftNode);
+            var left = _paramFinder.Parameters;
+            _paramFinder.Visit(rightNode);
+            var right = _paramFinder.Parameters;
+
+            if (left.All(p => p == _destinationParameter) && right.All(p => p == _sourceParameter))
+            {
+                _sourceMembers.Add(rightNode);
+                _destinationMembers.Add(leftNode);
+            }
+            if (left.All(p => p == _sourceParameter) && right.All(p => p == _destinationParameter))
+            {
+                _sourceMembers.Add(leftNode);
+                _destinationMembers.Add(rightNode);
+            }
+        }
+    }
+
+    internal class ParameterFinderVisitor : ExpressionVisitor
+    {
+        public IList<ParameterExpression> Parameters { get; private set; }
+
+        public override Expression Visit(Expression node)
+        {
+            Parameters = new List<ParameterExpression>();
+            return base.Visit(node);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            Parameters.Add(node);
             return node;
         }
     }
