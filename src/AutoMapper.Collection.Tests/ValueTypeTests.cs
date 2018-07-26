37a965bfd543ed46bd7ed22ef7003149eb8a8ff0
Patric Forsgard <patric@tasteful.se>
2018-07-26 10:20:49 +0200

    Adding value type tests

---
+++ src/AutoMapper.Collection.Tests/ValueTypeTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper.EquivalencyExpression;
+using Xunit;
+
+namespace AutoMapper.Collection
+{
+    public class ValueTypeTests
+    {
+        [Fact]
+        public void MapValueTypes()
+        {
+            Mapper.Reset();
+            Mapper.Initialize(c =>
+            {
+                c.AddCollectionMappers();
+
+                c.CreateMap<Country, CountryDto>()
+                    .ForMember(x => x.Nationalities, m => m.MapFrom(x => x.Persons))
+                    .ReverseMap();
+
+                c.CreateMap<int, PersonNationality>()
+                    .EqualityComparison((src, dest) => dest.NationalityCountryId == src);
+            });
+
+            var persons = new[]
+            {
+                new PersonNationality{PersonId = 1, NationalityCountryId = 101},
+                new PersonNationality{PersonId = 2, NationalityCountryId = 102},
+                new PersonNationality{PersonId = 3, NationalityCountryId = 103},
+                new PersonNationality{PersonId = 4, NationalityCountryId = 104},
+            };
+
+            var country = new Country { Persons = new List<PersonNationality>(persons) };
+            var countryDto = new CountryDto { Nationalities = new List<int> { 104, 103, 105 } };
+
+            Mapper.Map(countryDto, country);
+
+            Assert.NotStrictEqual(new[] { persons[3], persons[2], country.Persons.Last() }, country.Persons);
+            Assert.Equal(0, country.Persons.Last().PersonId);
+        }
+
+        public class PersonNationality
+        {
+            public int PersonId { get; set; }
+            public int NationalityCountryId { get; set; }
+        }
+
+        public class Country
+        {
+            public IList<PersonNationality> Persons { get; set; }
+        }
+
+        public class CountryDto
+        {
+            public IList<int> Nationalities { get; set; }
+        }
+    }
+}
