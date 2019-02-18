80d9046b39f95ac532c3f7a91514cba51253ec23
Patric Forsgard <patric@tasteful.se>
2019-02-18 19:42:22 +0100

    Use the AutoMapper Features function to add extension into the core.

---
+++ src/AutoMapper.Collection/Configuration/CollectionMappingExpressionFeature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using AutoMapper.Collection.Execution;
+using AutoMapper.EquivalencyExpression;
+
+namespace AutoMapper.Collection.Configuration
+{
+    public class CollectionMappingExpressionFeature<TSource, TDestination> : IMappingExpressionFeature
+    {
+        private readonly Expression<Func<TSource, TDestination, bool>> _expression;
+
+        public CollectionMappingExpressionFeature(Expression<Func<TSource, TDestination, bool>> expression)
+        {
+            _expression = expression;
+        }
+
+        public void Configure(TypeMap typeMap)
+        {
+            var equivalentExpression = new EquivalentExpression<TSource, TDestination>(_expression);
+            typeMap.Features.Set(new CollectionMappingFeature(equivalentExpression));
+        }
+
+        public IMappingExpressionFeature Reverse()
+        {
+            var reverseExpression = Expression.Lambda<Func<TDestination, TSource, bool>>(_expression.Body, _expression.Parameters[1], _expression.Parameters[0]);
+            return new CollectionMappingExpressionFeature<TDestination, TSource>(reverseExpression);
+        }
+    }
+}
